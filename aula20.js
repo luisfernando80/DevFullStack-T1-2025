51d38606ae8021b763513df01040a1a898580d14
luisfernando80 <lfernando.ti80@gmail.com>
2025-06-17 10:50:21 -0300

    atualizando ate o momento a pasta modulo02 com arquivos das aulas

---
+++ aula20.js
@@ -0,0 +1,25 @@
+// AULA 20 - 02 DE JUNHO DE 2025 - Segunda-Feira
+
+// GIT
+
+//********************************* GIT **********************************
+
+//  git --version => ver a versao instalada do git no pc;
+
+//  git init => inicializar um repositorio local;
+
+//  git remote add origin 'link do repositorio remoto' => add uma origem ao meu repositorio local, vincular o remoto com o local;
+
+//  git remote get-url origin => mostra qual url essa pasta esta vinculada no github;
+
+//  git config --global init.defaultBranch main => para sempre que iniciar uma nova branch ela vai ser main;
+
+//  git add .
+
+//  git status
+
+//  git commit -m 'meu primeiro commit'
+
+//  git log
+
+//  git push
---
+++ aula23.js
@@ -0,0 +1,5 @@
+// AULA 23 - 07 DE JUNHO DE 2025 - Sábado
+
+// FRONT-END
+
+//********************************* FRONT-END *******
